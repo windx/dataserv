eb4e154c87a8e26fe885c3b64bc7472e19e1f621
rainkid <rainkid@163.com>
2011-05-31 11:37:57 +0800

    commit

--- dataserv.php
+++ dataserv.php
@@ -1,16 +1,23 @@
 <?php
+//new the class
 $ds = new DataServ();
-
-$ds->sethost("127.0.0.1");
+//set iptions for mysql connect
+$ds->sethost("127.0.0.1"); 
 $ds->setuser("root");
 $ds->setpasswd("root");
 $ds->setdb("pw8");
-
 $ds->settable("pw_house_agency");
+
+/*set fields and value
+* rand int array("int",min,max)
+* rand string array("string",minlen,manlen)
+* string like "admin"
+* int like 0
+*/
 $ds->setfields(
 	  array("agencyid"=>"",
                 "name"=>array("int",20,50),
-                "address"=>array("int",20,50),
+                "address"=>array("string",20,50),
 	        "icon"=>"icon",
                 "bgimg"=>"bgimg",
 	        "mainbusiness"=>1,
@@ -19,5 +26,7 @@ $ds->setfields(
 		"vieworder"=>1
           )
 );
+//create data file
 $ds->create(1,1);
+//load data file into mysql
 $ds->loaddata();
